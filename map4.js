8f2a83de19b749393f4de9f7ab4a421105c9d5c7
MagedAZ <magedaz@moe.edu.kw>
2025-08-07 10:22:28 +0300

    Optimize map loading performance with parallel loading, resource hints, and better script loading

--- map4.js
+++ map4.js
@@ -5,46 +5,60 @@ var config = {
     port: 443,
     isSecure: true
 };
+
+// Optimize RequireJS configuration
 require.config({
-    baseUrl: (config.isSecure ? "https://" : "http://") + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
+    baseUrl: (config.isSecure ? "https://" : "http://") + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
+    waitSeconds: 30, // Increase timeout for slower connections
+    enforceDefine: false
 });
 
-require(["js/qlik"], function (qlik) {
+// Use DOMContentLoaded for faster initialization
+document.addEventListener('DOMContentLoaded', function() {
+    require(["js/qlik"], function (qlik) {
 
-    // Show spinner initially
-    document.getElementById('spinner').style.visibility = 'visible';
-    document.getElementById('spinner').style.display = 'block';
+        // Show spinner initially
+        const spinner = document.getElementById('spinner');
+        if (spinner) {
+            spinner.style.visibility = 'visible';
+            spinner.style.display = 'block';
+        }
 
-    qlik.on("error", function (error) {
-        $('#popupText').append(error.message + "<br>");
-        $('#popup').fadeIn(1000);
-    });
+        qlik.on("error", function (error) {
+            $('#popupText').append(error.message + "<br>");
+            $('#popup').fadeIn(1000);
+        });
 
-    $("#closePopup").click(function () {
-        $('#popup').hide();
-    });
+        $("#closePopup").click(function () {
+            $('#popup').hide();
+        });
 
-    var app = qlik.openApp('abe37f96-29b3-42f3-b45c-db41d2ab1320', config);
-    
-    app.clearAll();
-
-    function cs() {
+        var app = qlik.openApp('abe37f96-29b3-42f3-b45c-db41d2ab1320', config);
+        
         app.clearAll();
-    }
 
-   // $('.button').on('click', function () {
-   //     cs();
-//		console.log("clear button pressed");
-  //  });
+        function cs() {
+            app.clearAll();
+        }
 
-    app.getObject('QV02', 'HXJcjzA');
-    
-    // Get the map object and hide spinner when it's loaded
-    app.getObject('QV01', 'pHTVjT').then(function() {
-        // Hide spinner when map is loaded
-        document.getElementById('spinner').style.visibility = 'hidden';
-        document.getElementById('spinner').style.display = 'none';
-        console.log("Map loaded, removing spinner");
+        // Load objects in parallel for better performance
+        Promise.all([
+            app.getObject('QV02', 'HXJcjzA'),
+            app.getObject('QV01', 'pHTVjT')
+        ]).then(function() {
+            // Hide spinner when both objects are loaded
+            if (spinner) {
+                spinner.style.visibility = 'hidden';
+                spinner.style.display = 'none';
+            }
+            console.log("All objects loaded, removing spinner");
+        }).catch(function(error) {
+            console.error("Error loading objects:", error);
+            if (spinner) {
+                spinner.style.visibility = 'hidden';
+                spinner.style.display = 'none';
+            }
+        });
+
     });
-
 });
